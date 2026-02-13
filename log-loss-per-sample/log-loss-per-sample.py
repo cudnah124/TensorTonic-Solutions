8c99f4f11afb39b607b63ce1c210e0a0cb66839f
cudnah124 <cudnah.124@gmail.com>
2026-02-13 23:03:08 +0700

    Update solution for log-loss-per-sample

--- log-loss-per-sample/log-loss-per-sample.py
+++ log-loss-per-sample/log-loss-per-sample.py
@@ -5,16 +5,4 @@ def log_loss(y_true, y_pred, eps=1e-15):
     Compute per-sample log loss.
     """
     # Write code here
-
-    L = []
-
-    for y, y_p in zip(y_true, y_pred):
-        # clip trước
-        y_p = min(max(y_p, eps), 1 - eps)
-
-        loss = -(y * math.log(y_p) +
-                 (1 - y) * math.log(1 - y_p))
-
-        L.append(loss)
-
-    return L
+    return [-(y * math.log(min(max(y_p, eps), 1-eps)) + (1 - y) * math.log(1 - min(max(y_p, eps), 1-eps))) for y, y_p in zip(y_true, y_pred)]
