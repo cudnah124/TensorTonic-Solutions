8289ef9ff52c02723306c90b7ee6573240c89b16
cudnah124 <cudnah.124@gmail.com>
2026-02-13 23:23:23 +0700

    Update solution for catalog-coverage

--- catalog-coverage/catalog-coverage.py
+++ catalog-coverage/catalog-coverage.py
@@ -3,10 +3,5 @@ def catalog_coverage(recommendations, n_items):
     Compute the catalog coverage of a recommender system.
     """
     # Write code here
-    travel = set()
-
-    for recommendation in recommendations:
-        for item in recommendation:
-            travel.add(item)
-
+    travel = {item for rec in recommendations for item in rec}
     return len(travel) / n_items
