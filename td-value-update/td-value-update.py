6d45d66d75c11e86a6218a2d9632b0024874cd6d
cudnah124 <cudnah.124@gmail.com>
2026-02-21 20:54:35 +0700

    Add solution for td-value-update

---
+++ td-value-update/td-value-update.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def td_value_update(V, s, r, s_next, alpha, gamma):
+    """
+    Returns: updated value function V_new
+    """
+    # Write code here
+    V_new = V.copy()
+    
+    td_error = r + gamma * V[s_next] - V[s]
+
+    
+    V_new[s] = V[s] + alpha * td_error
+
+    return V_new
